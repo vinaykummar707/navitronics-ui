ef037d0c15fe93e9c7ec56c758a067c421020671
Vinay Kummar <vpanuganti@jetblue.com>
2025-01-26 08:09:00 +0530

    buildfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-port:3090
+  build: {
+    chunkSizeWarningLimit: 2000,
   },
-  plugins: [react(),tsConfigPaths()],
-})
+  server: {
+    port: 3090,
+  },
+  plugins: [react(), tsConfigPaths()],
+});
